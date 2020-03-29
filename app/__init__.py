e2c653ba1fc19dc785ec73724cd933980f5d5dc4
Krystall Messam <massem100@outlook.com>
2020-03-29 13:18:03 -0500

    init was linked to wrong database, updated

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "RABBITSFLYBIRDS SING"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:SQLpass@localhost/lab5"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:projectPASS@localhost/project1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
