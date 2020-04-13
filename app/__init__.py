d1422445e9f2205b854f77894cae7e3f90dc342d
Krystall Messam <massem100@outlook.com>
2020-04-12 22:34:01 -0500

    init.py updated

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "RABBITSFLYBIRDS SING"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql: // gtdxgzynorfyfb: eb9e877586aefb23db0561c7ef59c060569f84bab6ca5940c015bc80f94e6971@ec2-54-152-175-141.compute-1.amazonaws.com: 5432/d4ein5o6rtssfg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://znhmyqggjhvgxu:c599c02ecfb0f74edb722319dd1be537c0b795ed5e8b9ba7a4c7d11e15c87a3e@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d2fdqrtu7g88gk'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 
